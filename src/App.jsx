0454a834e23eb01951e1188d13d7d4ab3bfb1e7f
Soheil Moonesi <s.moonesi@alumni.znu.ac.ir>
2023-06-27 23:46:44 +0430

    third commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,29 @@
-// const data = [
-//   {
-//     // why must this object wrapped in array?
-//     name: "فتوسل 16 آمپر شیوا امواج",
-//     price: "239000",
-//     photo: "",
-//     Remaining: "10",
-//   },
-//   {
-//     name: " سوپر تایمر دیجیتال شیوا امواج",
-//     price: "657000",
-//     photo: "",
-//     Remaining: "20",
-//   },
-// ];
+const _data = [
+  {
+    name: "فتوسل 16 آمپر شیوا امواج",
+    price: "239000",
+    photo: "Models/فتوسل 16 آمپر شیوا امواج.jpg",
+    remaining: "10",
+  },
+
+  {
+    name: "تایمر دیجیتال شیوا امواج",
+    price: "657000",
+    photo: "Models/تایمر دیجیتال شیوا امواج.jpg",
+    remaining: "20",
+  },
+  {
+    name: "تایمر چپ گرد-راست گرد",
+    price: "686000",
+    photo: "Models/تایمر چپ گرد-راست گرد.png",
+    remaining: "15",
+  },
+];
+
+import propTypes from "prop-types";
 
 function App() {
   return (
-    // we must return just one variable
     <div className="w-screen h-screen bg-slate-500 flex flex-col justify-center items-center">
       <Header />
       <All_Models />
@@ -24,36 +31,62 @@ function App() {
     </div>
   );
 }
-// how to export and showing img?
-// answer : we must use public directory for export images
-function Models() {
-  return (
-    <div className="bg-red-500 flex flex-col w-52 h-72 m-3">
-      <img src="Models/فتوسل 16 آمپر شیوا امواج.jpg" alt="" />
-      <div>فتوسل 16 آمپر شیوا امواج</div>
-      <div>239000 </div>
-      <div>10</div>
-    </div>
-  );
-}
-function Header() {
-  return <h1>ecommerce name</h1>;
-}
 
 function All_Models() {
   return (
     <div>
       <h2 className="text-center">تمامی محصولات</h2>
       <div className="flex">
-        <Models
+        <div className="flex">
+          {_data.map((models) => (
+            <Models modelsObj={models} />
+          ))}
+        </div>
+
+        {/* <Models
           name="فتوسل 16 آمپر شیوا امواج"
           price="239000"
           photo="Models/فتوسل 16 آمپر شیوا امواج.jpg"
           remaining="10"
         />
+        <Models
+          name="تایمر دیجیتال شیوا امواج"
+          price="657000"
+          photo="Models/تایمر دیجیتال شیوا امواج.jpg"
+          remaining="20"
+        />
+        <Models
+          name="تایمر چپ گرد-راست گرد"
+          price="686000"
+          photo="Models/تایمر چپ گرد-راست گرد.png"
+          remaining="15"
+        /> */}
       </div>
     </div>
   );
+}
+
+function Models(props) {
+  console.log(props);
+
+  return (
+    <div className="bg-red-500 flex flex-col w-52 h-72 m-3">
+      <img src={props?.modelsObj?.photo} alt="" />
+      <div>{props?.modelsObj?.name}</div>
+      <div>{props?.modelsObj?.price} </div>
+      <div>{props?.modelsObj?.remaining}</div>
+    </div>
+  );
+}
+Models.propTypes = {
+  name: propTypes.string,
+  price: propTypes.number,
+  photo: propTypes.string,
+  remaining: propTypes.number,
+};
+
+function Header() {
+  return <h1>ecommerce site</h1>;
 }
 
 function Footer() {
