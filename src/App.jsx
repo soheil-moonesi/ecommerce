a9f8894fd3fba0cc37382cc42f36f08a26eb5cc7
Soheil Moonesi <s.moonesi@alumni.znu.ac.ir>
2023-06-29 13:33:05 +0430

    forth commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ const _data = [
     name: "فتوسل 16 آمپر شیوا امواج",
     price: "239000",
     photo: "Models/فتوسل 16 آمپر شیوا امواج.jpg",
-    remaining: "10",
+    remaining: "0",
   },
 
   {
@@ -20,7 +20,7 @@ const _data = [
   },
 ];
 
-import propTypes from "prop-types";
+// import propTypes from "prop-types";
 
 function App() {
   return (
@@ -31,72 +31,57 @@ function App() {
     </div>
   );
 }
-
+let listing_models = 1;
 function All_Models() {
   return (
     <div>
       <h2 className="text-center">تمامی محصولات</h2>
       <div className="flex">
-        <div className="flex">
-          {_data.map((models) => (
-            <Models modelsObj={models} />
-          ))}
-        </div>
-
-        {/* <Models
-          name="فتوسل 16 آمپر شیوا امواج"
-          price="239000"
-          photo="Models/فتوسل 16 آمپر شیوا امواج.jpg"
-          remaining="10"
-        />
-        <Models
-          name="تایمر دیجیتال شیوا امواج"
-          price="657000"
-          photo="Models/تایمر دیجیتال شیوا امواج.jpg"
-          remaining="20"
-        />
-        <Models
-          name="تایمر چپ گرد-راست گرد"
-          price="686000"
-          photo="Models/تایمر چپ گرد-راست گرد.png"
-          remaining="15"
-        /> */}
+        {listing_models > 0 ? (
+          <div className="flex">
+            {_data.map((models) => (
+              <Models modelsObj={models} key={models.name} />
+            ))}
+          </div>
+        ) : (
+          <p> محصولی برای نمایش وجود ندارد ، در حال انبار گردانی هستیم</p>
+        )}
       </div>
     </div>
   );
 }
 
-function Models(props) {
-  console.log(props);
-
+function Models(modelsObj) {
+  if (modelsObj?.remaining == 0) return null;
   return (
     <div className="bg-red-500 flex flex-col w-52 h-72 m-3">
-      <img src={props?.modelsObj?.photo} alt="" />
-      <div>{props?.modelsObj?.name}</div>
-      <div>{props?.modelsObj?.price} </div>
-      <div>{props?.modelsObj?.remaining}</div>
+      <img src={modelsObj?.photo} alt="" />
+      <div>{modelsObj?.name}</div>
+      <div>{modelsObj?.price} </div>
+      <div>{modelsObj?.remaining}</div>
     </div>
   );
 }
-Models.propTypes = {
-  name: propTypes.string,
-  price: propTypes.number,
-  photo: propTypes.string,
-  remaining: propTypes.number,
-};
 
 function Header() {
   return <h1>ecommerce site</h1>;
 }
 
 function Footer() {
+  const hour = new Date().getHours();
+  const activeHour = 8;
+  const deActiveHour = 22;
+  const isActive = hour >= activeHour && hour <= deActiveHour;
+
   return (
-    <footer>{new Date().toLocaleDateString("fa-IR")} :تاریخ امروز </footer>
+    <footer className="text-center">
+      {new Date().toLocaleDateString("fa-IR")} :تاریخ امروز
+      <div>
+        پشتیبانی تلفنی از ساعت {activeHour} تا {deActiveHour} : 09190169216
+      </div>
+      {isActive && <p>همین حالا تماس بگیرید</p>}
+    </footer>
   );
-  // const hour = new Date().getHours();
-  // const activeHour = 8;
-  // const deactiveHour =23;
-  // const isActive = hour >= openHour && hour <= closeHour;
 }
 
 export default App;
--- test.js
+++ test.js
@@ -1,23 +1,25 @@
-const data = [
-    {
-      name: "فتوسل 16 آمپر شیوا امواج",
-      price: "239000",
-      photo: "Models/فتوسل 16 آمپر شیوا امواج.jpg",
-      remaining: "10",
-    },
+// const data = [
+//     {
+//       name: "فتوسل 16 آمپر شیوا امواج",
+//       price: "239000",
+//       photo: "Models/فتوسل 16 آمپر شیوا امواج.jpg",
+//       remaining: "10",
+//     },
   
-    {
-      name: "تایمر دیجیتال شیوا امواج",
-      price: "657000",
-      photo: "Models/تایمر دیجیتال شیوا امواج.jpg",
-      remaining: "20",
-    },
-    {
-      name: "تایمر چپ گرد-راست گرد",
-      price: "686000",
-      photo: "Models/تایمر چپ گرد-راست گرد.png",
-      remaining: "15",
-    },
-  ];
-
-  
+//     {
+//       name: "تایمر دیجیتال شیوا امواج",
+//       price: "657000",
+//       photo: "Models/تایمر دیجیتال شیوا امواج.jpg",
+//       remaining: "20",
+//     },
+//     {
+//       name: "تایمر چپ گرد-راست گرد",
+//       price: "686000",
+//       photo: "Models/تایمر چپ گرد-راست گرد.png",
+//       remaining: "15",
+//     },
+//   ];
+const a = 5
+const b = 6
+let c = a+b
+console.log(c)
